b18afaa2d6339b1b7ef7b5725b2badd231af455d
Anjali Tomar <anjali.theaj3366@gmail.com>
2025-11-19 21:22:55 +0530

    Add deployed url

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -11,14 +11,14 @@ const Reservation = () => {
   const [email, setEmail] = useState("");
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
-  const [phone, setPhone] = useState(0);
+  const [phone, setPhone] = useState("");
   const navigate = useNavigate();
 
   const handleReservation = async (e) => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://aahar-food-app.vercel.app/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
@@ -30,7 +30,7 @@ const Reservation = () => {
       toast.success(data.message);
       setFirstName("");
       setLastName("");
-      setPhone(0);
+      setPhone("");
       setEmail("");
       setTime("");
       setDate("");
@@ -88,8 +88,8 @@ const Reservation = () => {
                   onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
-                  type="number"
-                  placeholder="98989898XX"
+                  type="tel"
+                  placeholder="+91 XXXXX XXXXX"
                   value={phone}
                   onChange={(e) => setPhone(e.target.value)}
                 />
